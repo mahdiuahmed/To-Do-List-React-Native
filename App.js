08617ede4ea9ae9ab504e5e611610964b7d7f235
Vanguard-Visuals <66173196+Vanguard-Visuals@users.noreply.github.com>
2022-11-16 23:53:49 +0000

    new proj

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons'
+import Tasks from './components/Tasks';
+
+
+const text = ['1', '2', '3', '4', '5'];
+
+const completeTask = (index) => {
+  let itemsCopy = [...text];
+  itemsCopy.splice(index, 1);
+  text(itemsCopy)
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>TO-DO</Text>
+        <View style={styles.items}>
+        {
+            text.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                  <Tasks text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  tasksWrapper: {
+paddingTop: 80,
+paddingHorizontal: 20,
+  },
+  sectionTitle: {
+fontSize: 24,
+fontWeight: 'bold'
+},
+  items: {
+
   },
 });
